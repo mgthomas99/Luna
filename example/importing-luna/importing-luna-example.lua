ac070abb2d9987e298c70e460291aef62ae29af2
George Thomas <mgthomas99@users.noreply.github.com>
2017-07-20 21:11:35 +0100

    Update importing-luna-example.lua

--- example/importing-luna/importing-luna-example.lua
+++ example/importing-luna/importing-luna-example.lua
@@ -5,3 +5,13 @@
 ]]
 package.path = package.path .. ";./../../../?.lua"
 Luna = require("Luna/luna")
+
+--[[
+  The following line is optional; the line brings
+  Luna's functions into the global scope, so that
+  we don't have to prepend every Luna function
+  with `Luna.` (For example, by doing `Luna.introduce()`,
+  we can begin to use `MyClass = class({})` instead
+  of `MyClass = Luna.class({})`)
+]]
+Luna.introduce()
