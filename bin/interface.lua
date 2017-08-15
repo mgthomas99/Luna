4815f428e1bf2de8ec7fa1c356e64affa8f75665
gthomas <gthomas@geologging.com>
2017-08-15 08:31:45 +0100

    type checking and super-interfaces

--- bin/interface.lua
+++ bin/interface.lua
@@ -10,8 +10,27 @@ end
 local Interface = {}
 
 function Interface.create(identifier, super, template)
+  if (type(identifier) == "string" and type(super) == "table" and type(template) == "table") then
+    -- All good!
+  elseif (type(identifier) == "string" and type(super) == "table") then
+    template = super
+    super = nil
+  elseif (type(identifier) == "table" and type(super) == "table") then
+    template = super
+    super = identifier
+    identifier = nil
+  elseif (type(identifier) == "table") then
+    template = identifier
+    identifier = nil
+  end
+
   local self = setmetatable({}, Interface)
 
+  if (super ~= nil) then
+    for k,v in pairs(super) do
+      self[k] = v
+    end
+  end
   for k,v in pairs(template) do
     if (type(v) ~= "string") then
       return error("An interface property must be a string!")
